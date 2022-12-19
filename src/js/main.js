1e1a7666ee9501002636b5d5b453760fde5053ad
ivuruDev <84606380+ivuru@users.noreply.github.com>
2022-12-19 12:02:37 +0900

    Create main.js

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+  /*=================================================
+  スマホメニュー
+  ===================================================*/
+  $('.toggle_btn').on('click', function() {
+    if ($('#header').hasClass('open')) {
+      $('#header').removeClass('open');
+    } else {
+      $('#header').addClass('open');
+    }
+  });
